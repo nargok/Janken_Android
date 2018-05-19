3c12b1f7cbef4a37a379c4304db0462c28a9e472
Ryoichi Gokan <nargok@gmail.com>
2018-05-20 01:04:23 +0900

    Initial commit

---
+++ app/src/main/java/sample/janken/MainActivity.kt
@@ -0,0 +1,34 @@
+package sample.janken
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it) }
+        choki.setOnClickListener { onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        // アプリケーション起動時にじゃんけん結果を初期化する
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+//        val intent = Intent(this, ResultActivity::class.java)
+//        intent.putExtra("MY_HAND", view?.id)
+//        startActivity(intent)
+        startActivity<ResultActivity>("MY_HAND" to view?.id)
+    }
+
+}
